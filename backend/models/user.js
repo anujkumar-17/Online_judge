eca54dbf6842d427f4d81c389602132889fbba03
anujkumar-17 <140160613+anujkumar-17@users.noreply.github.com>
2024-05-24 16:28:38 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,13 @@
 const mongoose=require('mongoose');
 
 const userSchema=new mongoose.Schema({
-    username:{type: String, default: null},
-    email:{type: String, unique: true},
-    password:{type: String},
-    birthYear:{type: Number},
-    country:{type: String}
+  username:{type: String,required: true},
+  email:{ type:String,required:true,unique: true,trim: true},
+  password:{ type: String,required: true },
+  birthYear:{ type: Number,required: true },
+  country:{ type: String,required: true },
 });
 
-module.exports=mongoose.model('User', userSchema);
+const User=mongoose.model('User', userSchema);
+
+module.exports=User;
