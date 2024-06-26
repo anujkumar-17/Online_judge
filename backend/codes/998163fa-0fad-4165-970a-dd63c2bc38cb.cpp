716e40149c7045e4235c0ada6e44e2678dd17408
anuj <anuj.1512k@gmail.com>
2024-06-26 11:36:45 +0530

    Welcome to my Online Judge application

---
+++ backend/codes/998163fa-0fad-4165-970a-dd63c2bc38cb.cpp
@@ -0,0 +1,30 @@
+
+    #include <bits/stdc++.h>
+    using namespace std;
+
+    bool isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        long long reversed = 0;
+        long long temp = x;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        if(reversed == x) return true;
+        else return false;
+    }
+    
+    int main() {
+      
+      // you can start writing your code from here
+      int x;cin>>x;
+      cout<<isPalindrome(x);
+      return 0;
+    }
+  
