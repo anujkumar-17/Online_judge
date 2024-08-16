cda675d1294e1d025ee44f7772b74deec1e28d76
anuj <anuj.1512k@gmail.com>
2024-08-16 19:35:19 +0530

    welcome to my online judge

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -6,7 +6,7 @@ const User = require('../models/user');
 const registerUser = async (req, res) => {
   try {
     const { username, email, password, birthYear, country } = req.body;
-    console.log('Received registration admin data:', { username, email, password, birthYear, country });
+    console.log('Received registration user data:', { username, email, password, birthYear, country });
 
     // Check if user already exists
     const existingUser = await User.findOne({ email });
@@ -26,8 +26,12 @@ const registerUser = async (req, res) => {
       country,
       role: 'user',
     });
-
     await newUser.save();
+    const token = jwt.sign(
+      { email: newUser.email },
+      'f3a95c1746c9b8991c2c30f7bbd6b1d91b36e11a1d4765040ef85b1c55ab768a', // Replace with your actual secret key
+      { expiresIn: '1h' } // Token expiration time (1 hour in this case)
+    );
     res.status(201).json({ message: 'User registered successfully', token });
   } catch (error) {
     console.error('Error during user registration:', error);
